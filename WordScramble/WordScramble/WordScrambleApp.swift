342c5b5273ae72356afb861ed4f7f6c28cfd59ae
Anurag Bhanderi <64243476+AnuragBhanderi@users.noreply.github.com>
2021-10-20 10:19:55 +0530

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anurag Bhanderi on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
